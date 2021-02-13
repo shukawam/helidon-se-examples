9308b36b677efc200347c29f79c764deba5c6ad5
s001-kawamura <s001.kawamura@gmail.com>
2021-02-13 16:45:06 +0900

    Update config service

--- src/main/java/shukawam/examples/helidon/se/config/ConfigService.java
+++ src/main/java/shukawam/examples/helidon/se/config/ConfigService.java
@@ -5,12 +5,9 @@ import io.helidon.webserver.Routing;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
-import shukawam.examples.helidon.se.greet.GreetService;
 
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
-import javax.json.JsonObject;
-import java.io.ObjectInputFilter;
 import java.util.Collections;
 import java.util.logging.Logger;
 
@@ -29,19 +26,10 @@ public class ConfigService implements Service {
     @Override
     public void update(Routing.Rules rules) {
         rules
-                .get("/1", this::getConfigHandler1)
-                .get("/2", this::getConfigHandler2);
+                .get("/", this::getConfigHandler);
     }
 
-    private void getConfigHandler1(ServerRequest req, ServerResponse res) {
-        var message = String.format("Config value is %s", configValue);
-        LOGGER.info(message);
-        var jsonObject = JSON.createObjectBuilder().add("message", configValue).build();
-        res.send(jsonObject);
-    }
-
-    private void getConfigHandler2(ServerRequest req, ServerResponse res) {
-        var config = Config.create();
+    private void getConfigHandler(ServerRequest req, ServerResponse res) {
         var message = String.format("Config value is %s", configValue);
         LOGGER.info(message);
         var jsonObject = JSON.createObjectBuilder().add("message", configValue).build();
